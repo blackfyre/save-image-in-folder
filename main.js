704555cb4460ab91992ec36ec0981ffde1b483de
Meki <gnick666@gmail.com>
2017-11-23 09:35:47 +0100

    added a few more stuff

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-(function () {
+browser.contextMenus.create({
+  id: "log-selection",
+  title: browser.i18n.getMessage("contextMenuItemSelectionLogger"),
+  contexts: ["selection"]
+}, onCreated);
 
-    if (window.simfHasRun) {
-        return;
-      }
-      window.simfHasRun = true;
-
-
-    browser.contextMenus.create({
-        id: "log-selection",
-        title: browser.i18n.getMessage("contextMenuItemSelectionLogger"),
-        contexts: ["selection"]
-      }, onCreated);
-})();
+console.log('fun stuff')
